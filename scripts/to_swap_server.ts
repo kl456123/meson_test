81f50832d4b80c6de6b7788855a57f27a27aa841
break.x <break.x@binance.com>
2024-06-28 17:02:50 +0800

    add to swap poc

--- scripts/to_swap_server.ts
+++ scripts/to_swap_server.ts
@@ -1,3 +1,40 @@
-async function main() {}
+enum BridgingStatus {
+  Failed,
+  Pending,
+  Success,
+}
+
+interface BridgingProgress {
+  status: BridgingStatus;
+}
+
+async function checkBridgingProgress(
+  sourceTxHash: string,
+  destTxhash: string,
+): Promise<BridgingProgress> {
+  return { status: BridgingStatus.Failed };
+}
+
+async function signByOracleSource(
+  bridginProgress: BridgingProgress,
+): Promise<string> {
+  return "";
+}
+
+async function swapOnToChainWithSignature(swapRequest: {}, signature: string) {}
+
+async function main() {
+  // check bridge event happened on source chain
+  const sourceTxHash = "";
+  const destTxhash = "";
+  const bridginProgress = await checkBridgingProgress(sourceTxHash, destTxhash);
+
+  // check tokens received on dest chain and the connection between source tx and dest tx
+  // oracle source sign signature for conformation
+  const signature = await signByOracleSource(bridginProgress);
+  // relayer swap with the oracle source's signature on dest chain to complete the bridge request totally
+  const swapRequest = {};
+  await swapOnToChainWithSignature(swapRequest, signature);
+}
 
 main();
